2cb4bd4d142a0f662008e1f73b7c03fa3074ad48
nszchagas <nszchagas@gmail.com>
2021-07-26 16:18:07 -0300

    removing unfinished files

--- criador_arquivos.py
+++
@@ -1,7 +0,0 @@
-entrada = ' '
-while entrada != 'sair':
-    entrada = input("Nome do arquivo:")
-    if entrada == 'sair':
-        break
-    with open (f'{entrada}.py', 'w') as f: 
-        f.write('# -*- coding: utf-8 -*-')
